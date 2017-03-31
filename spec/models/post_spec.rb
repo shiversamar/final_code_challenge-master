3a36e7751d07ef9d571d1ff5e7778e42e65c563f
Amar Shivers <shiversamar@yahoo.com>
2017-03-31 13:19:02 -0700

    views modification

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,25 +1,25 @@
 
-# require 'rails_helper'
-#
-# RSpec.describe Post, type: :model do
-#
-#   let(:post) {create(:post)}
-  #
-  # it 'has title and body' do
-  #   expect(post.title).to_not be_nil
-  #   expect(post.content).to_not be_nil
-  # end
-  #
-  # describe 'body association' do
-  #   let(:body_1) { build(:body, :no_post)}
-  #   let(:body_2) { build(:body, :no_post, body: "Before they sold out banjo etsy yr, vexillologist ...")}
-  #   it 'list body in the they were created' do
-  #     body_1.post_id = post.id
-  #     body_1.save!
-  #     body_2.post_id = post.id
-  #     body_2.save!
-  #     expect(post.body).to eq([body_1, body_2])
-  #   end
-  # end
+require 'rails_helper'
 
-# end#rspec
+RSpec.describe Post, type: :model do
+
+  let(:post) {create(:current_user.posts.build)}
+
+  it 'has title and body' do
+    expect(post.title).to_not be_nil
+    expect(post.content).to_not be_nil
+  end
+
+  describe 'body association' do
+    let(:body_1) { build(:body, :no_post)}
+    let(:body_2) { build(:body, :no_post, body: "Before they sold out banjo etsy yr, vexillologist ...")}
+    it 'list body in the they were created' do
+      body_1.post_id = post.id
+      body_1.save!
+      body_2.post_id = post.id
+      body_2.save!
+      expect(post.body).to eq([body_1, body_2])
+    end
+  end
+
+end#rspec
