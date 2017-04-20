7090dc9767d288ff657af822465c1785efe6b413
Amar Shivers <shiversamar@yahoo.com>
2017-04-19 18:23:34 -0700

    modified post nodel

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
 	validates :title, presence: true, length: {maximum: 100}
 	validates :body,  presence: true
 end
